6efa27a3930f3aae053d8435c9bff2393ba4743e
Sunny Chan <sunny.chan.developer@gmail.com>
2024-12-13 21:42:23 +0000

    Setting up Vite with Vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  function greet () {
+    alert("Hello");
+  }
+
+</script>
+
+<template>
+  <div>
+    <h3>Hello There! What is your name?</h3>
+    <input></input>
+    <button @click="greet">Say Hello</button>
+  </div>
+</template>
+
